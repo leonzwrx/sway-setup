41e9a9a94462024e087486a7252c5981ff68bffd
leonzwrx <158376547+leonzwrx@users.noreply.github.com>
2025-09-18 18:08:00 -0600

    all kinds of fedora changes

--- scripts/sway-install_fedora.sh
+++ scripts/sway-install_fedora.sh
@@ -9,9 +9,8 @@
 #
 # - This script will add some customization to a stock Fedora Sway spin
 # - Start with stock Fedora Sway spin with mostly defaults
-# - Verify internet connection
 # - Make sure sway is working and functional
-# - Make sure this repo is cloned into ~/Downloads
+# - Make sure this repo is cloned into $HOME/Downloads
 # - After running this script, clone/copy dotfiles to make sure sway/waybar customization gets copied
 
 set -e
@@ -24,112 +23,115 @@ fi
 
 # Set the username variable
 username=$(whoami)
-userhome="/home/$username"
 
 ##################################
 # Install sway-related packages
 ##################################
 
 prepare_environment() {
-    cd $userhome/Downloads/sway-setup/scripts
+    cd $HOME/Downloads/sway-setup/scripts
     chmod +x *.sh
 }
 
 install_sway_packages() {
-    # Sway input configurator using pip:
-    sudo pip install sway-input-config
-
+  # pipx packages (not available in standard repos):
+    sudo dnf -y install pipx
+    pipx install sway-input-config
+    
     # Autotiling
-    sudo pip install autotiling
+    pipx install autotiling
 
-    # Install nwg-look - UPDATE
-    if [ -f $userhome/Downloads/sway-setup/scripts/nwg-look.sh ]; then
-        bash $userhome/Downloads/sway-setup/scripts/nwg-look.sh
-    fi
+    # rofimoji using pipx because the standard repo has a package that needs rofi-wayland but sticking with regular rofi
+    pipx install rofimoji
 
     # Networking and bluetooth
     sudo dnf install -y blueman nm-connection-editor NetworkManager-tui network-manager-applet nm-connection-editor-desktop NetworkManager 
 
-    # Clipboard and screenshot tools
-    sudo dnf install -y grim slurp wl-clipboard swappy
+    # Clipboard, background and screenshot tools
+    sudo dnf install -y grim slurp wl-clipboard swappy azote
 
     # Theming
     sudo dnf install -y qt5-qtstyleplugins qt5ct qt6ct papirus-icon-theme kvantum
-    #Install Nord-Kvantum theme
-    mkdir -p $userhome/.config/Kvantum
-    tar -xzf "$userhome/Downloads/sway-setup/resources/Nord-Kvantum.tar.gz" -C $userhome/.config/Kvantum
 
-    # More Sway utilities
-    sudo dnf install -y rofi-wayland foot ffmpegthumbnailer jq khal mako tumbler waybar xsettingsd xdg-desktop-portal-wlr python3-send2trash inotify-tools kanshi
+    #Nord-Kvantum theme
+    echo "Installing Nord-Kvantum theme..."
+    mkdir -p "$HOME/.config/Kvantum"
+    # Using a more robust tar command that handles nested directories better
+    tar -xzvf "$HOME/Downloads/sway-setup/resources/Nord-Kvantum.tar.gz" -C "$HOME/.config/Kvantum"
 
-    # Emoji selector - can also be installed with pip install rofimoji
-    sudo dnf install -y rofimoji
+    # More Sway utilities and related Wayland packages
+    sudo dnf install -y rofi foot ffmpegthumbnailer jq khal mako tumbler waybar xsettingsd xdg-desktop-portal-wlr \
+    python3-send2trash inotify-tools kanshi mako wofi Xwayland wlsunset wtype
+
+    #OPTIONAL Emoji selector - has a rofi-wayland requirement. 
+    #Installing using pipx instead
+    #sudo dnf install -y rofimoji
+
+    #nwg-piotr packages from main reposs
+    sudo dnf install -y nwg-bar nwg-wrapper
+
+    # Optional: rofi-wayland fork in case standard package does not work
+    # sudo dnf install -y rofi-wayland
 }
 
 install_manual_sway_packages() {
-  # wttrbar - Could not get the script to dynamically download the latest version. This script downloads a specific version (can change in the script).
-    # Alternative solution, download the binary from https://github.com/bjesus/wttrbar/releases/latest and place into /usr/bin manually
-    cd $userhome/Downloads/sway-setup/scripts
-    sudo bash wttrbar.sh
+    # wttrbar
+    echo "Installing wttrbar from source..."
+    bash "$HOME/Downloads/sway-setup/scripts/wttrbar.sh"
+    
+    
+    # nwg-piotr packages from copr not found in main repos
+    sudo dnf -y copr enable tofik/nwg-shell
+    sudo dnf -y update
+    sudo dnf install -y nwg-displays nwg-look
 
-    # Azote for backgrounds - Manually moved .desktop and icon from azote/dist folder
-    cd $userhome/SourceBuilds
-    git clone https://github.com/nwg-piotr/azote.git
-    cd azote
-    sudo python3 setup.py install
+    #gtklock
+    sudo dnf -y copr enable mochaa/gtklock
+    sudo dnf -y update
+    sudo dnf -y install gtklock
+
+
+    #nwg-clipman from source (not available in standard repos)
+    mkdir -p  $HOME/SourceBuilds && cd $HOME/SourceBuilds
+    git clone https://github.com/nwg-piotr/nwg-clipman.git
+    cd nwg-clipman/
+    sudo ./install.sh 
 
     # swayr
     cargo install swayr
     cargo install cargo-update
 
-    # nwg-bar - could not build from source but it's available as Fedora Copr
-    sudo dnf -y copr enable tofik/nwg-shell
-    sudo dnf update
-    sudo dnf install -y nwg-bar nwg-displays
-    sudo dnf -y copr disable tofik/nwg-shell
-
-    # gtklock - not available in Fedora's repos
-    sudo dnf -y copr enable mochaa/gtklock
-    sudo dnf update
-    sudo dnf install -y gtklock gtk-session-lock
-    sudo dnf -y corp disable mochaa/gtklock
-
-    #Clipboard stuff - cliphist and nwg-clipman (GUI tool)
+    # cliphist
     go install go.senan.xyz/cliphist@latest
-    
-    cd $userhome/SourceBuilds
-    git clone https://github.com/nwg-piotr/nwg-clipman
-    cd nwg-clipman
-    sudo bash install.sh
-
-    #[OPTIONAL] Uncomment to copy .desktop files for manually installed applications
-    #sudo cp $userhome/Downloads/sway-setup/scripts/resources/*.desktop /usr/share/applications
-
 }
 
 install_custom_systemd_services() {
     # Install custom systemd user services
     # Ensure script has write permissions (optional)
-    # chmod +w $userhome/.config/systemd/user  # Uncomment if needed
-    cd "$userhome/Downloads/sway-setup/resources"  # Double quotes for safety
-    
-    # Check if directory exists (avoid overwriting data accidentally)
-    if [[ -d "$userhome/.config/systemd/user" ]]; then
+    # chmod +w $HOME/.config/systemd/user  # Uncomment if needed
+    cd "$HOME/Downloads/sway-setup/resources"
+
+    # Ensure systemd user directory exists
+    mkdir -p "$HOME/.config/systemd/user"
+
     # Copy service files with verbose output
-      cp -v swayidle-inhibit.service "$userhome/.config/systemd/user"
-      cp -v swayidle.service "$userhome/.config/systemd/user"
-      cp -v waybar.service "$userhome/.config/systemd/user"
-      cp -v swayrd "$userhome/.config/systemd/user"
-    else
-      echo "Warning: Directory ~/.config/systemd/user does not exist. Skipping copy."
-    fi
+    cp -v swayidle-inhibit.service "$HOME/.config/systemd/user/"
+    cp -v swayidle.service "$HOME/.config/systemd/user/"
+    cp -v waybar.service "$HOME/.config/systemd/user/"
+    cp -v swayrd.service "$HOME/.config/systemd/user/"
 
-sudo systemctl daemon-reload  # Reload systemd
-systemctl --user enable waybar.service --now
-systemctl --user enable swayrd.service --now
-systemctl --user enable swayidle.service --now
-systemctl --user enable swayidle-inhibit.service --now
+    systemctl --user daemon-reload  # Reload systemd
+    systemctl --user enable waybar.service
+    systemctl --user enable swayrd.service
+    systemctl --user enable swayidle.service
+    systemctl --user enable swayidle-inhibit.service
 
+    echo "Installing sway-systemd from source..."
+    bash "$HOME/Downloads/sway-setup/scripts/sway-systemd.sh" 
+
+
+
+ }
 
 final_cleanup() {
     sudo dnf autoremove
